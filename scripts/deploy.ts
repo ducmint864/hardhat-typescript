875f192f3eeeed6af180b3668929c171de74c727
duckmint864 <nddminh2021@gmail.com>
2023-07-07 12:18:41 +0700

    finished basic config, successfully deployed smart contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,24 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const Price = await ethers.deployContract("Price");
+    const PriceAddress = await Price.getAddress();
 
-  const lockedAmount = ethers.parseEther("0.001");
+    await Price.waitForDeployment();
+    
+    const EscrowFactory = await ethers.getContractFactory("Escrow", 
+    {
+        libraries: {
+            Price: PriceAddress
+        }
+    })
+    const Escrow = await EscrowFactory.deploy();
+    await Escrow.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    const EscrowAddress = await Escrow.getAddress();
+    console.log(`Escrow smart contract has been deployed to address: ${EscrowAddress}\n`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main().catch((err: any) => {
+    console.log(err);
+})
---
+++ test/Escrow.ts
@@ -0,0 +1,12 @@
+import { assert } from "chai";
+import { ethers } from "hardhat";
+
+describe("Escrow", function() {
+    this.beforeEach(async function() {
+        const Escrow = await ethers.deployContract("Escrow");
+    }) 
+
+    it("", async funcion() {
+        
+    })
+})
