ad0bc423a5f00f1a373eeff0b5e7bfc7d84bca04
duckmint864 <nddminh2021@gmail.com>
2023-07-08 10:53:07 +0700

    Added "AggregatorV3Interace _price_feed" as the parameter for Fund

---
+++ contracts/Fund.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "./Price.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract Fund
+{
+    AggregatorV3Interface price_feed;
+    uint256 public constant MINIMUM_USD = 50;
+    address public immutable OWNER;
+    address[] public funders;
+    
+    mapping(address=>uint256) public AddressToAmount; 
+    
+    constructor(AggregatorV3Interface _price_feed) {
+        price_feed = _price_feed;
+        OWNER = msg.sender;
+    }
+
+    function fund() public payable {
+        require(Price.convertToUSD(price_feed, msg.value) >= MINIMUM_USD, "Minimum amount not reached! Transaction has been reverted");
+        funders.push(msg.sender);
+        AddressToAmount[msg.sender] += msg.value;
+    }
+
+    function withdraw() public OnlyOwner {
+        for (uint256 i=0; i<funders.length; i++) {
+            AddressToAmount[funders[i]] = 0;
+        }
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("Here is your fund withdrawal, please don't spend it on personal purposes");
+        require(callSuccess, "Cannot withdraw fund");
+    }
+
+    modifier OnlyOwner {
+        require(msg.sender == OWNER, "Only the owner of this funding is allowed to withdraw to funded money!");
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
--- contracts/Price.sol
+++ contracts/Price.sol
@@ -4,17 +4,13 @@ pragma solidity ^0.8.9;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library Price {
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface pricefeed;
-        pricefeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        ); // ETH/USD pricefeed for the Goerli testnet
-        (, int256 price, , , ) = pricefeed.latestRoundData();
+    function getPrice(AggregatorV3Interface _price_feed) public view returns (uint256) {            
+        (, int256 price, , , ) = _price_feed.latestRoundData();
         return uint256(price) / 1e8;
     }
 
-    function convertToUSD(uint256 amount) public view returns (uint256) {
-        return (amount * getPrice()) / 1e18;
+    function convertToUSD(AggregatorV3Interface _price_feed, uint256 amount) public view returns (uint256) {
+        return (amount * getPrice(_price_feed)) / 1e18;
     }
 
     function Uint256ToString(uint256 value) public pure returns (string memory) {
