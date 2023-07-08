7cda8d9e0b76d79e08eb196413e7ff09788c2068
duckmint864 <nddminh2021@gmail.com>
2023-07-08 11:16:43 +0700

    added helper-configs folder to store additional config files. Added first NetworkConfig.ts to store the price feed addresses of different networks, so that we can directly access them during contract deployment and gets the correct price of whatever network the contract is being deployed to

---
+++ helper-configs/NetworkConfig.ts
@@ -0,0 +1,10 @@
+const netWorkConfig = {
+    "31337" : { 
+        priceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306" // borrow from sepolia testnet
+    },
+    "11155111": { // sepolia_test_net
+        priceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
+    }
+}
+
+export default netWorkConfig;
