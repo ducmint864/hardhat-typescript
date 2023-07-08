c6eee18cbd47d401fb4435fa1a9f183312779bb1
duckmint864 <nddminh2021@gmail.com>
2023-07-08 12:04:41 +0700

    Update approriate typescript config

--- helper-configs/NetworkConfig.ts
+++ helper-configs/NetworkConfig.ts
@@ -1,8 +1,8 @@
 const netWorkConfig = {
-    "31337" : { 
+    31337: { 
         priceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306" // borrow from sepolia testnet
     },
-    "11155111": { // sepolia_test_net
+    11155111: { // sepolia_test_net
         priceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
     }
 }
