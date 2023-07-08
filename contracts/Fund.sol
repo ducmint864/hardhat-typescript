7dbd5f40c50a9b7f6bc6788948d1a63e483bc068
duckmint864 <nddminh2021@gmail.com>
2023-07-09 01:05:21 +0700

    Modified: renamed a few variables, changed modifier and function declaration order to follow the 'Solidity Style Guides'.

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -1,49 +1,49 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.9;
 
 import "./Price.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract Fund
-{
+contract Fund {
     uint256 public constant MINIMUM_USD = 50;
     address public immutable OWNER;
     address[] public funders;
-    mapping(address=>uint256) public AddressToAmount; 
-    AggregatorV3Interface immutable public priceFeed;    
-    
+    mapping(address => uint256) public addressToAmount;
+    AggregatorV3Interface public immutable priceFeed;
+
+    modifier OnlyOwner() {
+        require(
+            msg.sender == OWNER,
+            "Only the owner of this funding is allowed to withdraw to funded money!"
+        );
+        _;
+    }
+
     constructor(address priceFeedAddress) {
         priceFeed = AggregatorV3Interface(priceFeedAddress);
         OWNER = msg.sender;
     }
 
     function fund() public payable {
-        require(Price.convertToUSD(priceFeed, msg.value) >= MINIMUM_USD, "Minimum amount not reached! Transaction has been reverted");
+        require(
+            Price.convertToUSD(priceFeed, msg.value) >= MINIMUM_USD,
+            "Minimum amount not reached! Transaction has been reverted"
+        );
         funders.push(msg.sender);
-        AddressToAmount[msg.sender] += msg.value;
+        addressToAmount[msg.sender] += msg.value;
     }
 
     function withdraw() public OnlyOwner {
-        for (uint256 i=0; i<funders.length; i++) {
-            AddressToAmount[funders[i]] = 0;
+        for (uint256 i = 0; i < funders.length; i++) {
+            addressToAmount[funders[i]] = 0;
         }
         funders = new address[](0);
 
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("Here is your fund withdrawal, please don't spend it on personal purposes");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }(
+            "Here is your fund withdrawal, please don't spend it on personal purposes"
+        );
         require(callSuccess, "Cannot withdraw fund");
-    }
-
-    modifier OnlyOwner {
-        require(msg.sender == OWNER, "Only the owner of this funding is allowed to withdraw to funded money!");
-        _;
-    }
-
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
     }
 }
--- contracts/Price.sol
+++ contracts/Price.sol
@@ -4,16 +4,23 @@ pragma solidity ^0.8.9;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library Price {
-    function getPrice(AggregatorV3Interface _price_feed) public view returns (uint256) {            
+    function getPrice(
+        AggregatorV3Interface _price_feed
+    ) public view returns (uint256) {
         (, int256 price, , , ) = _price_feed.latestRoundData();
         return uint256(price) / 1e8;
     }
 
-    function convertToUSD(AggregatorV3Interface _price_feed, uint256 amount) public view returns (uint256) {
+    function convertToUSD(
+        AggregatorV3Interface _price_feed,
+        uint256 amount
+    ) public view returns (uint256) {
         return (amount * getPrice(_price_feed)) / 1e18;
     }
 
-    function Uint256ToString(uint256 value) public pure returns (string memory) {
+    function uint256ToString(
+        uint256 value
+    ) public pure returns (string memory) {
         if (value == 0) {
             return "0";
         }
