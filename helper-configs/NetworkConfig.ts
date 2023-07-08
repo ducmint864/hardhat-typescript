83c1359c1f3ea06e98733180dce2c60a9a095cdf
duckmint864 <nddminh2021@gmail.com>
2023-07-08 19:25:31 +0700

    added property name for networks

--- helper-configs/NetworkConfig.ts
+++ helper-configs/NetworkConfig.ts
@@ -1,8 +1,10 @@
 const networkConfig = {
     31337: { 
+        name: "hardhat_localhost",
         priceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306" // borrow from sepolia testnet
     },
     11155111: { // sepolia_test_net
+        name: "sepolia_testnet",
         priceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
     }
 }
