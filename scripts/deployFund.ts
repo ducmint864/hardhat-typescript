0c47a78d79fcff02e4ce75746251ee52cbd4c27a
duckmint864 <nddminh2021@gmail.com>
2023-07-09 12:49:38 +0700

    Modified: turn scripts/deployFund.ts script into modular script that can be invoked from outside(the deployFund() function of scripts/deployFund.ts can now handles errors in its own function body

--- scripts/deployFund.ts
+++ scripts/deployFund.ts
@@ -6,40 +6,45 @@ import { developmentChains } from "../helper-configs/NetworkConfig";
 import deployMockV3Aggregator from "./test/deployMock";
 
 
+// function to deploy the Fund smart contract
 export default async function deployFund() {
-    const chainId: number = network.config.chainId! ?? process.env.DEFAULT_CHAIN_ID;
-    let priceFeedAddress;
+    try {
 
-    console.log("---------------------------- Contract deployment script ----------------------------\n");
-    console.log("--> Network: {\n\tName: ", networkConfig[chainId as keyof typeof networkConfig].name);
-    console.log("\tChain-Id: ", chainId);
-    console.log("}");
-    console.log("------------------------------------------------------------------------------------");
-    
+        const chainId: number = network.config.chainId! ?? process.env.DEFAULT_CHAIN_ID;
+        let priceFeedAddress;
 
-    if (developmentChains.includes(chainId)) {
-        console.log("--> Local host detected! Using MockV3Aggregator contract");
-        priceFeedAddress = deployMockV3Aggregator();
-    }
-    else {
-        priceFeedAddress = networkConfig[chainId as keyof typeof networkConfig].priceFeedAddress;
-    }
+        console.log("---------------------------- Contract deployment script ----------------------------\n");
+        console.log("--> Network: {\n\tName: ", networkConfig[chainId as keyof typeof networkConfig].name);
+        console.log("\tChain-Id: ", chainId);
+        console.log("}");
+        console.log("------------------------------------------------------------------------------------");
 
-    const PriceContract = await ethers.deployContract("Price");
-    await PriceContract.waitForDeployment();
-    const PriceContractAddress = await PriceContract.getAddress();
-    const FundFactory = await ethers.getContractFactory("Fund",
-        {
-            libraries: {
-                Price: PriceContractAddress
-            }
+
+        if (developmentChains.includes(chainId)) {
+            console.log("--> Local host detected! Using MockV3Aggregator contract");
+            priceFeedAddress = deployMockV3Aggregator();
         }
-    )
-    const FundContract: Fund = await FundFactory.deploy(priceFeedAddress); // only parameter: the price feed address of the network this contract is being deployed to
-    await FundContract.waitForDeployment();
-    console.log(`Fund contract has been deployed to address ${await FundContract.getAddress()}`);
+        else {
+            priceFeedAddress = networkConfig[chainId as keyof typeof networkConfig].priceFeedAddress;
+        }
+
+        const PriceContract = await ethers.deployContract("Price");
+        await PriceContract.waitForDeployment();
+        const PriceContractAddress = await PriceContract.getAddress();
+        const FundFactory = await ethers.getContractFactory("Fund",
+            {
+                libraries: {
+                    Price: PriceContractAddress
+                }
+            }
+        )
+        const FundContract: Fund = await FundFactory.deploy(priceFeedAddress); // only parameter: the price feed address of the network this contract is being deployed to
+        await FundContract.waitForDeployment();
+        console.log(`Fund contract has been deployed to address ${await FundContract.getAddress()}`);
+    } catch (err: any) {
+        console.log(err);
+    }
 }
 
-deployFund().catch((err: any) => {
-    console.log(err);
-})
+// Invoke contract deployment function
+deployFund();
