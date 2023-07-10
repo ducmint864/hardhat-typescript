bacb519e6bae3f2bec4dfce20173015b5e9a44c4
duckmint864 <nddminh2021@gmail.com>
2023-07-10 22:40:29 +0700

    Added: test suite for fund() function Fund contract

--- test/unit/Fund.test.ts
+++ test/unit/Fund.test.ts
@@ -2,34 +2,53 @@ import { ethers } from "hardhat";
 import { assert, expect} from "chai";
 import deployFund from "../../scripts/deployFund";
 import { Fund } from "../../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("Fund", async () => {
-    let response : string[];
-    let fundAddress : string;
-    let priceFeedAddress : string;
-    let fund : Fund;
+    let RESPONSE : string[];
+    let FUND_ADDRESS : string;
+    let PRICE_FEED_ADDRESS : string;
+    let FUND : Fund;
+    let ACCOUNTS : HardhatEthersSigner[];
+    let DEPLOYER : HardhatEthersSigner;
+    let FUNDER : HardhatEthersSigner;
+    const SEND_AMOUNT : bigint = ethers.parseEther("0.1")
 
     before(async() => {
-        response = await deployFund() ?? ["", ""];
-        fundAddress = response[0];
-        priceFeedAddress = response[1];
-
-        it("-Deploy the Fund contract without issues", () => {
-            assert.notEqual(fundAddress, "")
-        })
-
-        it("-Deploy the MockV3Aggregator contracdt without issues", () => {
-            assert.notEqual(priceFeedAddress, "");
-        })
+        RESPONSE = await deployFund() ?? ["", ""];
+        [FUND_ADDRESS, PRICE_FEED_ADDRESS] = RESPONSE;
+        ACCOUNTS = await ethers.getSigners();
+        [DEPLOYER, FUNDER] = await ethers.getSigners();
+        FUND = await ethers.getContractAt("Fund", FUND_ADDRESS);
     })
 
-    beforeEach(async () => {
-        fund = await ethers.getContractAt("Fund", fundAddress);
-    })
-
-    describe("Constructor", async() => {
+    describe("constructor()", async() => {
         it("-Initializes Fund contract with correct price feed address", async() => {
-            assert.equal(await fund.priceFeed(), priceFeedAddress);
+            assert.equal(await FUND.priceFeed(), PRICE_FEED_ADDRESS);
+        })
+    })
+
+    describe("fund()", async() => {
+        let funderContract : Fund;
+        
+        before(() => {
+            funderContract = FUND.connect(FUNDER);
+        })
+
+        it ("Fails if you don't fund enough ETH", async () => {
+            await expect(FUND.fund()).to.be.reverted;
+        })
+
+        it ("Store the correct amount of ETH that each funder has funded", async () => {
+            funderContract.fund({value : SEND_AMOUNT});
+            assert.equal(SEND_AMOUNT, await funderContract.addressToAmount(FUNDER.address));
+        })
+
+        it("Only add a funder to the funders array once", async () => {
+            // send ETH 2 more times
+            funderContract.fund({value : SEND_AMOUNT});
+            funderContract.fund({value : SEND_AMOUNT});
+            assert.equal(1, (await funderContract.funders()).length);
         })
     })
 })
