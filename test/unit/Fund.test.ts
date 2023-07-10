e6fa3e897bf5b6dcde50c2ff92ba67053a3ef845
duckmint864 <nddminh2021@gmail.com>
2023-07-10 14:56:35 +0700

    Added: test for the constructor of the contract

--- test/unit/Fund.test.ts
+++ test/unit/Fund.test.ts
@@ -1,9 +1,35 @@
 import { ethers } from "hardhat";
 import { assert, expect} from "chai";
 import deployFund from "../../scripts/deployFund";
+import { Fund } from "../../typechain-types";
 
-describe("Fund", () => {
+describe("Fund", async () => {
+    let response : string[];
+    let fundAddress : string;
+    let priceFeedAddress : string;
+    let fund : Fund;
+
+    before(async() => {
+        response = await deployFund() ?? ["", ""];
+        fundAddress = response[0];
+        priceFeedAddress = response[1];
+
+        it("-Deploy the Fund contract without issues", () => {
+            assert.notEqual(fundAddress, "")
+        })
+
+        it("-Deploy the MockV3Aggregator contracdt without issues", () => {
+            assert.notEqual(priceFeedAddress, "");
+        })
+    })
+
     beforeEach(async () => {
-        deployFund();
+        fund = await ethers.getContractAt("Fund", fundAddress);
+    })
+
+    describe("Constructor", async() => {
+        it("-Initializes Fund contract with correct price feed address", async() => {
+            assert.equal(await fund.priceFeed(), priceFeedAddress);
+        })
     })
 })
