5f3ef336bb110b0245736bcc8308729c57887e07
duckmint864 <nddminh2021@gmail.com>
2023-07-10 13:27:47 +0700

    Improved error handling. Modified: deployFund() now returns an array of 2 elements: address of Fund contract, address of MockV3Aggregator contract

--- scripts/deployFund.ts
+++ scripts/deployFund.ts
@@ -5,13 +5,13 @@ import networkConfig from "../helper-configs/NetworkConfig"
 import { developmentChains } from "../helper-configs/NetworkConfig";
 import deployMockV3Aggregator from "./test/deployMock";
 
+export let priceFeedAddress : string;
 
 // function to deploy the Fund smart contract
 export default async function deployFund() {
     try {
 
         const chainId: number = network.config.chainId! ?? process.env.DEFAULT_CHAIN_ID;
-        let priceFeedAddress;
 
         console.log("---------------------------- Contract deployment script ----------------------------\n");
         console.log("--> Network: {\n\tName: ", networkConfig[chainId as keyof typeof networkConfig].name);
@@ -22,10 +22,14 @@ export default async function deployFund() {
 
         if (developmentChains.includes(chainId)) {
             console.log("--> Local host detected! Using MockV3Aggregator contract");
-            priceFeedAddress = deployMockV3Aggregator();
+            priceFeedAddress = await deployMockV3Aggregator() ?? "";
         }
         else {
-            priceFeedAddress = networkConfig[chainId as keyof typeof networkConfig].priceFeedAddress;
+            priceFeedAddress = networkConfig[chainId as keyof typeof networkConfig].priceFeedAddress ?? "";
+        }
+
+        if (priceFeedAddress == "") {
+            throw new Error ("Cannot get address of MockV3Aggregator price feed");
         }
 
         const PriceContract = await ethers.deployContract("Price");
@@ -41,7 +45,7 @@ export default async function deployFund() {
         const FundContract: Fund = await FundFactory.deploy(priceFeedAddress); // only parameter: the price feed address of the network this contract is being deployed to
         await FundContract.waitForDeployment();
         console.log(`Fund contract has been deployed to address ${await FundContract.getAddress()}`);
-        return await FundContract.getAddress();
+        return [await FundContract.getAddress(), priceFeedAddress];
     } catch (err: any) {
         console.log("--> Error deploying Fund contract: ", err);
     }
